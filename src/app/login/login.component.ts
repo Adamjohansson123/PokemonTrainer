183e882c39d0a7ec464227e0f5415b52504422d6
wrang308 <markus.wrang_94@hotmail.com>
2021-08-23 11:44:28 +0200

    made so the login button redirects you to the catalogur page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 //export class LoginComponent implements OnInit {
   export class LoginComponent {
-  constructor() { }
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
     
   }
 
   handleLogin(event: Event): void  {
+    //if("username" !== ''){
+    if(true){  
+    localStorage.clear();
+      localStorage.setItem('username', "username");
+      this.route.navigate(['/catalogue'])
+    }
     console.log("loggin in" + event)
   }
 
